10d417aa4a5cced618c79ed1403b8e7f902b50ae
Breno Robert <brenorobert@ucl.br>
2021-11-17 19:12:53 -0300

    First commit with some basic things to use dotnet

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Charming_house
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
